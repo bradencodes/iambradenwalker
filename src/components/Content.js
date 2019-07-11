30a4015cd64ad176af14ab53d43d6d3fd4523ed8
Braden Walker <bradencodes@gmail.com>
2019-07-11 13:29:13 -0700

    scroll snapping working sometimes

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,8 +4,24 @@ const Content = () => {
   return (
     <div id='content-container'>
       <div className='content'>
-        <div id='iam'>I AM BRADEN WALKER</div>
-        <div id='full'>A FULL STACK DEVELOPER</div>
+        <div id='iam'>
+          I AM <span className='invisible'>BRADEN WALKER</span>
+        </div>
+        <div className='first'>
+          <span className='invisible'>I AM </span>BRADEN WALKER
+        </div>
+
+        <div className='introText'>
+          <span className='invisible'>I AM A </span>FULL STACK DEVELOPER
+        </div>
+
+        <div className='introText'>
+          <span className='invisible'>I AM </span>CREATIVE
+        </div>
+
+        <div className='introText'>
+          <span className='invisible'>I WILL </span>MAKE YOU MONEY
+        </div>
       </div>
 
       <div id='vignette' />
