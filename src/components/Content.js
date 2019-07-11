8948e54b0b509b92b7eac9c4786e3dbdfb77ce42
Braden Walker <bradencodes@gmail.com>
2019-07-10 18:47:58 -0700

    fix spacing between divs

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Content = () => {
   return (
     <div id='content-container'>
-      <div id='vignette' />
       <div className='content'>
         <div id='iam'>I AM BRADEN WALKER</div>
         <div id='full'>A FULL STACK DEVELOPER</div>
       </div>
+      <div id='vignette' />
     </div>
   );
 };
