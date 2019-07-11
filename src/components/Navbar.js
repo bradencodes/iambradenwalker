12fc918ce4b42e483ea94e44b1ea3a2d3efe81a7
Braden Walker <bradencodes@gmail.com>
2019-07-10 23:08:25 -0700

    fix navbar hover

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div id="navbar-container">
-      <div className="nav-link">
-        <div className="link-dot"></div>
-        <div className="link-text">Intro</div>
+    <div id='navbar-container'>
+      <div className='nav-link'>
+        <div className='link-dot' />
+        <div className='link-text'>Intro</div>
       </div>
-      <div className="nav-link">
-        <div className="link-dot"></div>
-        <div className="link-text">Projects</div>
+      <div className='nav-link'>
+        <div className='link-dot' />
+        <div className='link-text'>Projects</div>
       </div>
-      <div className="nav-link">
-        <div className="link-dot"></div>
-        <div className="link-text">Contact</div>
+      <div className='nav-link'>
+        <div className='link-dot' />
+        <div className='link-text'>Contact</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
