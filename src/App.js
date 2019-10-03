a5a34b1af4c07f8abefd9d26188df250acc31f21
Braden Walker <bradencodes@gmail.com>
2019-10-03 10:17:36 -0700

    init css scroll-snap behavior

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/main.css';
+
+import Intro from './components/Intro';
+
+function App() {
+  return (
+    <div className='App scrollSnap'>
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <>
+      <div className='scrollerSection'>
+        <div className='introText'>I AM BRADEN WALKER</div>
+      </div>
+      <div className='scrollerSection'>
+        <div className='introText'>I AM A FULL STACK DEVELOPER</div>
+      </div>
+      <div className='scrollerSection'>
+        <div className='introText'>I AM CREATIVE</div>
+      </div>
+    </>
+  );
+};
+
+export default Intro;
