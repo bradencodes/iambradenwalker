1f2268b58346fcdb57268b3873d15abd1937c50a
Braden Walker <bradencodes@gmail.com>
2019-10-06 17:11:40 -0700

    work on nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Nav = ({ ratio }) => {
   return (
     <div id='navContainer' className={ratio.isWide ? 'sideNav' : 'topNav'}>
-      
-    </div>
-  )
-}
+      <div className='background' />
 
-export default Nav
+      <div className='spacingHelper' />
+
+      <div className='optionContainer'>
+        <div className='dot filled' />
+        <span className='text focused'>Intro</span>
+      </div>
+
+      <div className='optionContainer'>
+        <div className='dot' />
+        {/* <span className='text'>Projects</span> */}
+      </div>
+
+      <div className='optionContainer'>
+        <div className='dot' />
+        {/* <span className='text'>Contact</span> */}
+      </div>
+
+      <div className='spacingHelper' />
+    </div>
+  );
+};
+
+export default Nav;
